be854896f761005a9d467068dc5056a9c0a5b61f
Matteo Albinola <malbinola@sonicle.com>
2019-02-13 18:28:45 +0100

    Turn off some unwanted php log entries

--- src/admin-cli.php
+++ src/admin-cli.php
@@ -1,5 +1,11 @@
 <?php
 
+error_reporting(E_ALL & ~E_NOTICE);
+// Tweaking the php error_reporting is not sufficient, z-push will internally
+// z-push will internally reset the configuration to E_ALL.
+// We need to set also this customized const...
+define('LOG_ERROR_MASK', ~(E_NOTICE|E_STRICT));
+
 require_once 'version.php';
 require_once 'vendor/autoload.php';
 
@@ -12,8 +18,3 @@ if (php_sapi_name() !== 'cli') {
 	die('<h1>'.$ZPUSH_TARGET_SCRIPT.' must NOT be called as web-page --> exiting !!!</h1>');
 }
 include WT_EAS_ROOT.'/inc/zpush.php';
-
-//$_SERVER['SCRIPT_FILENAME'] = __DIR__.$ZPUSH_SRC.'/'.$ZPUSH_SCRIPT;
-//chdir(__DIR__.$ZPUSH_SRC);
-//define('ZPUSH_CONFIG', __DIR__.'/inc/config.php');
-//include(__DIR__.$ZPUSH_SRC.'/'.$ZPUSH_SCRIPT);
--- src/index.php
+++ src/index.php
@@ -1,5 +1,11 @@
 <?php
 
+error_reporting(E_ALL & ~E_NOTICE);
+// Tweaking the php error_reporting is not sufficient, z-push will internally
+// z-push will internally reset the configuration to E_ALL.
+// We need to set also this customized const...
+define('LOG_ERROR_MASK', ~(E_NOTICE|E_STRICT));
+
 require_once 'version.php';
 require_once 'vendor/autoload.php';
 
