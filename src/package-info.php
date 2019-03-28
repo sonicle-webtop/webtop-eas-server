cf09eb8111ffe15e92a8ca8de53cfbbdac5feb3f
Matteo Albinola <malbinola@sonicle.com>
2019-03-28 17:54:39 +0100

    Set zpush version

--- src/package-info.php
+++ src/package-info.php
@@ -1,4 +1,6 @@
 <?php
 
+$v = \WT\Util::readVersion();
+define('ZPUSH_VERSION', substr($v, 0, strrpos($v, '.')));
 define('WEBTOP-EAS-SERVER_NAME', 'webtop-eas-server');
-define('WEBTOP-EAS-SERVER_VERSION', \WT\Util::readVersion());
+define('WEBTOP-EAS-SERVER_VERSION', $v);
